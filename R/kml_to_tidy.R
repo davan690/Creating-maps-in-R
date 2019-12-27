92333911f553f8ba2c8dfc81aabf714ef3609d96
Anthony R. Davidson <anthony.davidson@canberra.edu.au>
2019-12-27 15:01:29 +1100

    sorting tidykml stuff.

--- R/kml_to_tidy.R
+++ R/kml_to_tidy.R
@@ -6,28 +6,36 @@ library(leaflet)
 library(leaflet.extras)
 library(geojsonio)
 
-dat.loc <- c("C://Code/Creating-maps-in-R/data/invasive-species-NAH-research.kml")
-
+dat.loc <- c("C://Phd/beech-anova-simple/data/doc.kml")
+# dat.loc <- c("C://Code/Creating-maps-in-R/data/invasive-species-location-research-DATA.kmz")
 #if kmz file
-# unzip(zipfile = "C://Code/Creating-maps-in-R/data/invasive species location research.kmz")
-
-# xml.doc <- XML::xmlToList("doc.kml")
+# unzip(zipfile = "C://Code/Creating-maps-in-R/data/invasive-species-location-research-DATA.kmz.zip")
+# xml.doc <- XML::xmlToList("./doc.kml")
 # xml.doc
-# xml.locations <- xml2::xml_length("C://Code/Creating-maps-in-R/data/invasive-species-location-research.kml")
+
+#doesnt work
+# library(rgdal)
+# x = readOGR("C://Code/Creating-maps-in-R/data/invasive-species-location-research-DATA.kmz")
+
+
+# #using sf
+# library(sf)
+# #> Linking to GEOS 3.6.1, GDAL 2.1.3, PROJ 4.9.3
+# dat.st <- st_read("./doc.kml", )
+# 
+# xml.locations <- xml2::xml_length(only_elements = "./doc.kml")
 # tibble(xml.locations)
-
-#using sf
-library(sf)
-#> Linking to GEOS 3.6.1, GDAL 2.1.3, PROJ 4.9.3
-dat.st <- st_read("./data/invasive-species-NAH-research.kml")
-
-glimpse(dat.st)
+# 
+# glimpse(dat.st)
 
 # using package from github
-
 library(tidykml)
 
-tidykml::kml_bounds("./data/invasive-species-NAH-research.kml")
-tidykml::kml_info("./data/invasive-species-NAH-research.kml")
+map.bounds <- tidykml::kml_bounds("./data/doc.kml")
+map.info <- tidykml::kml_info("./data/doc.kml")
 
-tidy.kat <- tidykml::kml_points("./data/invasive-species-NAH-research.kml")
+tidy.kat <- tidykml::kml_points("./data/doc.kml")
+tidy.poly <- tidykml::kml_polygons("./doc.kml")
+
+tidy.read <- tidykml::kml_read("./doc.kml")
+
